fa5966e6d8d0b1eb5e9846f7ac3da37729d3a4ba
asia <abc@gmail.com>
2022-08-10 22:23:04 +0530

    java program changed

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -17,5 +17,15 @@ public class App
     private final String getMessage() {
         return message;
     }
+class FactorialExample{  
+  
+  int i,fact=1;  
+  int number=5;//It is the number to calculate factorial    
+  for(i=1;i<=number;i++){    
+      fact=fact*i;    
 
+}     
+  System.out.println("Factorial of "+number+" is: "+fact);    
+  
+} 
 }
