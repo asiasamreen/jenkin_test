1d4aa07768da6560186532514b583ebcf1f6f57b
asia <abc@gmail.com>
2022-08-10 22:35:12 +0530

    compilation error rectified

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -6,26 +6,12 @@ package com.mycompany.app;
 public class App
 {
 
-    private final String message = "Hello World!";
-
-    public App() {}
-
-    public static void main(String[] args) {
-        System.out.println(new App().getMessage());
-    }
-
-    private final String getMessage() {
-        return message;
-    }
-class FactorialExample{  
-  
+  public static void main(String args[]){  
   int i,fact=1;  
   int number=5;//It is the number to calculate factorial    
   for(i=1;i<=number;i++){    
       fact=fact*i;    
-
-}     
-  System.out.println("Factorial of "+number+" is: "+fact);    
-  
-} 
+  }    
+  System.out.println("Factorial of "+number+" is: "+fact);  
+    }
 }
